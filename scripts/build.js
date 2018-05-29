8be03ef9fbaca3cd6d4f522e5c98b467c54a9156
Christian Linchausen <cflinchausen@gmail.com>
2018-05-29 22:11:12 +0200

    Update Todos

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,12 @@
 /**
- * Link css with HTML
  * 3. Working Layouts
+ * Link css with HTML
  * 4. Working Partials
  * 5. Read Config
  * 6. Read Data
  * 7. Site Metadata
  * 8. Hot Reload
  * 9. Auto Deploy
- * 11. Git + gitignore
  * Asset loading
  * Favicon
  */
